30ce306fcc4170e0b9e879feaf08e55dabb397c4
Maitri Patel <maitriupatel2006@gmail.com>
2026-05-09 23:02:06 +0530

    Create StudentSystem.java

---
+++ StudentSystem.java
@@ -0,0 +1,17 @@
+# Student Management System
+
+This is a simple Java project based on OOP concepts.
+
+## Features
+- Add Student
+- Display Students
+- Search Student
+- Remove Student
+
+## Technologies Used
+- Java
+- ArrayList
+- OOP Concepts
+
+## Author
+Maitri Patel
